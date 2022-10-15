e3815f89bc794cd9c1967e47781432a59ab900f4
Arianna <arigcald@espol.edu.ec>
2022-10-14 19:51:51 -0500

    Actualizando Streamlit

---
+++ Proyecto.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Welcome to software for petroleum engineer')
+
+st.header("---")
+
+st.write('This app is going to be used as the kickoff lecture of this subject')
